4ba9a5278aab7891b56e39a094ca68d851b01f91
alesya.kolbik <moyaalesia@mail.ru>
2017-11-10 17:13:09 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String sourceText = "test 1111111 string.";
+        int counter = 0;
+        for (int i = 1; i < sourceText.length(); i++) {
+            if (sourceText.charAt(i) == sourceText.charAt(i - 1))
+                counter++;
+        }
+            if(counter>=4)
+                System.out.print("Есть пять одинаковых знаков подряд.");
+            else if(counter<=4)
+                System.out.print("Нет пяти одинаковых знаков подряд.");
+            else
+                counter=0;
+    }
+}
